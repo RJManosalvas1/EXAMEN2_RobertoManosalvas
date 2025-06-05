b924c275a95f0876fd6caa5a302b4ca236bdabaa
RJManosalvas1 <roberto.manosalvas@udla.edu.ec>
2025-06-05 08:53:52 -0500

    Final commit

--- EXAMEN2_RobertoManosalvas/Views/jokesPage.xaml.cs
+++ EXAMEN2_RobertoManosalvas/Views/jokesPage.xaml.cs
@@ -1,9 +1,46 @@
+using System.Net.Http;
+using System.Net.Http.Json;
+
 namespace EXAMEN2_RobertoManosalvas.Views;
 
 public partial class jokesPage : ContentPage
 {
-	public jokesPage()
-	{
-		InitializeComponent();
-	}
+    private readonly HttpClient _httpClient;
+
+    public jokesPage()
+    {
+        InitializeComponent();
+        _httpClient = new HttpClient();
+        Chiste();
+    }
+
+    private async void OnAnotherJokeButton(object sender, EventArgs e)
+    {
+        await Chiste();
+    }
+
+    private async Task Chiste()
+    {
+        try
+        {
+            var joke = await _httpClient.GetFromJsonAsync<Joke>("https://official-joke-api.appspot.com/random_joke");
+            if (joke != null)
+            {
+                SetupLabel.Text = joke.Setup;
+                PunchlineLabel.Text = joke.Punchline;
+            }
+        }
+        catch (Exception ex)
+        {
+            SetupLabel.Text = "Error al cargar el chiste.";
+            PunchlineLabel.Text = ex.Message;
+        }
+    }
+
+    public class Joke
+    {
+        public string Type { get; set; }
+        public string Setup { get; set; }
+        public string Punchline { get; set; }
+    }
 }
